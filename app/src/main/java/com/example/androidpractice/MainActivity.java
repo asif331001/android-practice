52becd58541c6a00156ee540f838c200af1fadbd
asif331001 <asif95331001@gmail.com>
2023-02-14 13:05:18 +0600

    Date Picker

--- app/src/main/java/com/example/androidpractice/MainActivity.java
+++ app/src/main/java/com/example/androidpractice/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.androidpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private DatePicker datePicker;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textid);
+        button = (Button) findViewById(R.id.buttonid);
+        datePicker = findViewById(R.id.datepickerid);
+
+        textView.setText(currentDate());
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                textView.setText(currentDate());
+            }
+        });
+    }
+    String currentDate(){
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("Your Selected Date: ");
+
+        stringBuilder.append(datePicker.getDayOfMonth()+"/");
+        stringBuilder.append((datePicker.getMonth()+1)+"/");
+        stringBuilder.append(datePicker.getYear());
+
+        return stringBuilder.toString();
     }
 }
