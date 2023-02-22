83d38f8b0141a31b91c990e0aa1bb1eed20fee21
asif331001 <asif95331001@gmail.com>
2023-02-22 11:44:36 +0600

    Progress bar

--- app/src/main/java/com/example/androidpractice/MainActivity.java
+++ app/src/main/java/com/example/androidpractice/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.androidpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    int progress;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressbarid);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                doWork();
+            }
+        });
+        thread.start();
+    }
+
+    public void doWork(){
+
+        for (progress=10; progress<=100; progress=progress+10){
+
+
+            try {
+                Thread.sleep(1000);
+                progressBar.setProgress(progress);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 }
